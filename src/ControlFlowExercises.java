a6dcfa64a3bb9f533d0e76726230ab91ec5c82a2
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-11-09 10:44:07 -0700

    Completed 2a. bullet #1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,21 @@
 public class ControlFlowExercises {
     public static void main(String [] args){
         //Exercise 1a.
-        int i = 5;
-        while (i <= 15){
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //Exercise 1b.
+        int i = 0;
+        do {
             System.out.println(i);
-            i++;
-        }
+            i += 2;
+        } while (i <= 100);
+
+
+
+
     }
 }
