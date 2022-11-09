fb8435234408efbe63e3bc60a0f770c78b37aa98
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-11-09 11:03:58 -0700

    Completed 1b Bullet #3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,32 @@
 public class ControlFlowExercises {
     public static void main(String [] args){
         //Exercise 1a.
-//        int i = 5;
-//        while (i <= 15){
-//            System.out.println(i);
-//            i++;
+//        int a = 5;
+//        while (a <= 15){
+//            System.out.println(a);
+//            a++;
 //        }
 
-        //Exercise 1b.
-        int i = 100;
-        do {
-            System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+        //Exercise 1b. bullet #1
+//        int b = 0;
+//        do {
+//            System.out.println(b);
+//            b += 2;
+//        } while (b <= 100);
 
+        //Exercise 1b. bullet #2
+//        int c = 100;
+//        do {
+//            System.out.println(c);
+//            c -= 5;
+//        } while (c >= -10);
+
+        //Exercise 1b. bullet #3
+        long d = 2;
+        do {
+            System.out.println(d);
+            d *= d;
+        } while (d < 1000000000);
 
 
 
