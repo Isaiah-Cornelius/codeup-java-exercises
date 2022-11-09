4682a8630e49b389e9e263165e1baf28e1e9d924
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-11-09 10:40:55 -0700

    Completed Exercise 1a

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,10 @@
+public class ControlFlowExercises {
+    public static void main(String [] args){
+        //Exercise 1a.
+        int i = 5;
+        while (i <= 15){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
